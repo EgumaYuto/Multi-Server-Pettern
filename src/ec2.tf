dcb878e1b78539c08153d3820d57b6656ab8b7fb
cabos <yuto.eguma@gmail.com>
2019-03-27 00:21:40 +0900

    add key pair setting

--- src/ec2.tf
+++ src/ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "app1" {
     ami = "ami-0f9ae750e8274075b"
     instance_type = "t2.micro"
 
+    ## TODO remove this
+    key_name = "myFirstKey"
+
     tags = {
         Name = "${terraform.workspace}-app1"
     }
@@ -11,6 +14,9 @@ resource "aws_instance" "app1" {
 resource "aws_instance" "app2" {
     ami = "ami-0f9ae750e8274075b"
     instance_type = "t2.micro"
+
+    ## TODO remove this
+    key_name = "myFirstKey"
 
     tags = {
         Name = "${terraform.workspace}-app2"
